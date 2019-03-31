a05051973d36a5ad145df3ed705f14268e01e384
wilever <wilevergomez@gmail.com>
2019-03-31 13:34:52 -0300

    company list load database data

--- src/main/webapp/app/wizzard/component.ts
+++ src/main/webapp/app/wizzard/component.ts
@@ -7,6 +7,10 @@ import { CollectionViewer, SelectionChange } from '@angular/cdk/collections';
 import { FlatTreeControl } from '@angular/cdk/tree';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, merge, Observable } from 'rxjs';
+import { ICompany } from 'app/shared/model/company.model';
+import { CompanyService } from '../entities/company/company.service';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 
 /** Flat node with expandable and level information */
 export class DynamicFlatNode {
@@ -127,11 +131,14 @@ export interface Tile {
     providers: [DynamicDatabase]
 })
 export class WizzardComponent implements OnInit {
-    company: string;
+    companies: ICompany[];
+    company: ICompany;
+    // company: string;
     mode = new FormControl('over');
     shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
     myControl = new FormControl();
     options: string[] = ['One', 'Two', 'Three'];
+    // companies: string[] = ['company_One', 'company_Two', 'company_Three'];
     filteredOptions: Observable<string[]>;
     favoriteSeason: string;
     seasons: string[] = ['Winter', 'Spring', 'Summer', 'Autumn'];
@@ -158,11 +165,40 @@ export class WizzardComponent implements OnInit {
 
     hasChild = (_: number, _nodeData: DynamicFlatNode) => _nodeData.expandable;
 
-    constructor(private _formBuilder: FormBuilder, database: DynamicDatabase) {
+    constructor(
+        private _formBuilder: FormBuilder,
+        database: DynamicDatabase,
+        private companyService: CompanyService,
+        private jhiAlertService: JhiAlertService
+    ) {
         this.treeControl = new FlatTreeControl<DynamicFlatNode>(this.getLevel, this.isExpandable);
         this.dataSource = new DynamicDataSource(this.treeControl, database);
 
         this.dataSource.data = database.initialData();
+    }
+
+    loadAllCompany() {
+        this.companyService
+            .query({
+                page: 0,
+                size: 20,
+                sort: null
+            })
+            .subscribe(
+                (res: HttpResponse<ICompany[]>) => this.paginateCompanies(res.body, res.headers),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
+
+    protected paginateCompanies(data: ICompany[], headers: HttpHeaders) {
+        this.companies = data;
+        if (this.company == null && this.companies) {
+            this.company = this.companies[0];
+        }
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 
     setStep(index: number) {
@@ -187,6 +223,7 @@ export class WizzardComponent implements OnInit {
         this.secondFormGroup = this._formBuilder.group({
             secondCtrl: ['', Validators.required]
         });
+        this.loadAllCompany();
     }
 
     private _filter(value: string): string[] {
